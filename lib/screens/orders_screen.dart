4f5247d41b978bd720a396dec851808ce0ad17ea
seanka <and.seanka@gmail.com>
2022-12-15 19:50:48 +0700

    added order screens, added intl package

---
+++ lib/screens/orders_screen.dart
@@ -0,0 +1,25 @@
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+
+import '../widgets/order_item.dart';
+import '../providers/orders.dart' show Orders;
+
+class OrdersScreen extends StatelessWidget {
+  const OrdersScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    final orderData = Provider.of<Orders>(context);
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Your Orders'),
+      ),
+      body: ListView.builder(
+        itemBuilder: (ctx, i) => OrderItem(
+          orderData.orders[i],
+        ),
+        itemCount: orderData.orders.length,
+      ),
+    );
+  }
+}
