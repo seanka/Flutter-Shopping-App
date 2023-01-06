fe6dadf45939cbeb81e77d3df82e5cabca6b6673
seanka <and.seanka@gmail.com>
2023-01-06 11:21:43 +0700

    added logout button

--- lib/providers/auth.dart
+++ lib/providers/auth.dart
@@ -68,4 +68,11 @@ class Auth with ChangeNotifier {
   Future<void> signin(String email, String password) async {
     return _authenticate(email, password, 'signInWithPassword');
   }
+
+  void logout() {
+    _token = '';
+    _userId = '';
+    _expiryDate = DateTime.fromMillisecondsSinceEpoch(0);
+    notifyListeners();
+  }
 }
--- lib/widgets/app_drawer.dart
+++ lib/widgets/app_drawer.dart
@@ -1,7 +1,9 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 
 import '../screens/user_products_screen.dart';
 import '../screens/orders_screen.dart';
+import '../providers/auth.dart';
 
 class AppDrawer extends StatelessWidget {
   const AppDrawer({super.key});
@@ -35,6 +37,14 @@ class AppDrawer extends StatelessWidget {
             onTap: () => Navigator.of(context)
                 .pushReplacementNamed(UserProductsScreen.routeName),
           ),
+          const Divider(),
+          ListTile(
+              leading: const Icon(Icons.exit_to_app),
+              title: const Text('Log Out'),
+              onTap: () {
+                Navigator.of(context).pop();
+                Provider.of<Auth>(context).logout();
+              }),
         ],
       ),
     );
