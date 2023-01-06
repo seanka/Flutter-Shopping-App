44d3225963d2de9597d904ad6dd500c60971d86b
seanka <and.seanka@gmail.com>
2023-01-06 12:17:44 +0700

    pushed home navigation when logout

--- lib/widgets/app_drawer.dart
+++ lib/widgets/app_drawer.dart
@@ -43,6 +43,7 @@ class AppDrawer extends StatelessWidget {
               title: const Text('Log Out'),
               onTap: () {
                 Navigator.of(context).pop();
+                Navigator.of(context).pushReplacementNamed('/');
                 Provider.of<Auth>(context).logout();
               }),
         ],
